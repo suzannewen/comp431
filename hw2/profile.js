c797a552ff5a334454f4cfdd782f98fcbc2889af
Suzanne Wen <suz.wen@yahoo.com>
2016-09-08 13:57:54 -0500

    added regex validation

--- hw2/profile.js
+++ hw2/profile.js
@@ -2,6 +2,7 @@ var valuesList = ["display", "email", "phone", "zipcode", "pw", "pwConfirm"]; //
 var currentValues = [];
 var inputArray = [];
 var alertValues = ["display name", "email", "phone number", "zipcode", "password", "confirmation password"];
+var regexValues = [".{1,}","[0-9a-zA-Z\-]{1,}.?[0-9a-zA-Z\-]*@[0-9a-zA-Z\-]{1,}.[a-zA-Z\-]{1,}","^\d{3}-\d{3}-\d{4}","[0-9]{5}", ".{6,12}", ".{6,12}"]
 
 window.onload = function () {
   valuesList.forEach(getCurrent);
@@ -17,8 +18,8 @@ function inputValues (element, index, array) { // fills inputArray with inputted
 }
 
 function pwMatch () { // user must have the same password in both fields
-  var pw = document.getElementById("password").value;
-  var match = document.getElementById("confirmation").value;        
+  var pw = document.getElementById("pwInput").value;
+  var match = document.getElementById("pwConfirmInput").value;        
 
   if (pw != match) {
     alert("Password does not match!");
@@ -49,7 +50,17 @@ function pwMatch () { // user must have the same password in both fields
   }
 
   function checkDiff (element, index, array) { // array is inputArray
+    // if (array[index].match(regexValues[indexs])) {
+
+    // }
+
+
     if (array[index].length > 0 && array[index] !== currentValues[index]) { // checks if input field is filled in and if it is different from the previous value
+      if (array[index].match(regexValues[index]) === null) { // checks if input is valid with regex
+        console.log("Your input for " + alertValues[index] + " is not valid.");
+        return;
+      }
+
       alertString += alertValues[index] + " ";
       changeIds.push(valuesList[index]);
       newValues.push(array[index]);

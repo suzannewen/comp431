5edf411867dedae976f0200d6d7c56a9cd855f86
Suzanne Wen <suz.wen@yahoo.com>
2016-09-08 21:55:17 -0500

    added comments

--- inclass-6/skyline.js
+++ inclass-6/skyline.js
@@ -32,6 +32,7 @@ var createApp = function(canvas) {
   var build = function() { 
 
     c.clearRect(0,0,800,800);
+
     // Create the ground
     var floor = canvas.height/2
     var grad = c.createLinearGradient(0,floor,0,canvas.height)
@@ -45,22 +46,18 @@ var createApp = function(canvas) {
     c.arc(sunX,75,50,0,2*Math.PI);
     c.fillStyle="yellow";
     c.fill();
-    sunX += 10;
-
-    // common size for windows
-    var windowSpacing = 2, floorSpacing = 3
-    var windowHeight = 5, windowWidth = 3
-
-    // colors of buildings
-    var blgColors = [ 'red', 'blue', 'gray', 'orange'] 
+    sunX += 10; // to move sun, skips to left of screen if it reaches the other side
+    if (sunX === 800) {
+      sunX = 0;
+    }
 
     var x0 = Math.random()*canvas.width
     var blgWidth = (windowWidth+windowSpacing) * Math.floor(Math.random()*10)
     var blgHeight = Math.random()*canvas.height/2
     c.fillStyle= blgColors[ Math.floor(Math.random()*blgColors.length)]
 
-    var blgDetails = [x0, blgWidth, blgHeight, c.fillStyle]; // create array to store details of this building
-    blgList.push(blgDetails);
+    var blgDetails = [x0, blgWidth, blgHeight, c.fillStyle]; // create array to store details of each building, this allows for all buildings to be rebuilt
+    blgList.push(blgDetails); // each building stored in an array
 
     blgList.forEach(function (element, index, array) {
       var x0 = array[index][0];
@@ -74,7 +71,7 @@ var createApp = function(canvas) {
       for (var y = floor - floorSpacing; y > floor - blgHeight; y -= floorSpacing + windowHeight) {
         for (var x = windowSpacing; x < blgWidth - windowWidth; x += windowSpacing + windowWidth) {
 
-          var randomInt = Math.round(Math.random()); // windows are randomly on or off
+          var randomInt = Math.round(Math.random()); // randomInt to determine if windows are on or off (yellow or black)
           if (randomInt === 1) {
             c.fillStyle = "yellow";
           }
@@ -82,18 +79,18 @@ var createApp = function(canvas) {
             c.fillStyle = "black";
           }
 
-          c.fillRect(x0 + x, y - windowHeight, windowWidth, windowHeight) // create windows for building
+          c.fillRect(x0 + x, y - windowHeight, windowWidth, windowHeight)
         }
     }
   });
 
-    var bmwImg = new Image();
+    var bmwImg = new Image(); // image object for car
     bmwImg.onload=function(){
         c.drawImage(bmwImg, carX, 380, 100, 80);
     }
     bmwImg.src = "bmw.jpg";
-    carX -= 20;
-    if (carX === 0) {
+    carX -= 20; // each time, the car will move left 20px
+    if (carX === 0) { // to detect if the car has reached the left of the screen
       carX = 800;
     }
   }

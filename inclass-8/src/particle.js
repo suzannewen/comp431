10dd13f78e62396558258e92d146cd63caefed5a
Suzanne Wen <suz.wen@yahoo.com>
2016-09-16 09:44:50 -0500

    added curly braces

--- inclass-8/src/particle.js
+++ inclass-8/src/particle.js
@@ -2,12 +2,12 @@ const random = (min=0, max=800) =>
     Math.random() * (max - min) + min
 
 // default values
-const particle = (
+const particle = ({
     mass=random(5, 30),
     position=[random(), random()],
     velocity=[random(-0.1, 0.1), random(-0.1, 0.1)],
     acceleration=[0, 0]
-) => {
+}) => {
     return {acceleration, velocity, position, mass}
 }
 
@@ -16,10 +16,10 @@ const update = ({acceleration, velocity, position, mass}, delta, canvas) => {
     position[0] += velocity[0]*delta + 0.5*acceleration[0]*delta*delta;
     position[1] += velocity[1]*delta + 0.5*acceleration[1]*delta*delta;
 
-    if (position[0] > 800) {
+    if (position[0] > 800 || position[0] < 0) {
       position[0] = 0;
     }
-    if (position[1] > 800) {
+    if (position[1] > 800 || position[1] < 0) {
       position[1] = 0;
     }
 

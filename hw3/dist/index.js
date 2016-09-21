5424cd2b87b06e2414a7a89cc215d1dd915cf3b4
Suzanne Wen <suz.wen@yahoo.com>
2016-09-20 19:29:18 -0500

    initial commit

---
+++ hw3/dist/index.js
@@ -0,0 +1,61 @@
+var today = new Date();
+
+function validateDOB () {
+  var birthDate = document.getElementById("birthdate").value;
+  var birthYear = birthDate.substr(0,4);
+  var birthMonth = birthDate.substr(5,2); 
+  var birthDay = birthDate.substr(8,2);
+
+  var checkDate = new Date(birthYear,birthMonth,birthDay);
+  var dateDiff = (today-checkDate)/(1000*60*60*24*365); //convert ms to years
+
+  if (dateDiff < 18) {
+    alert("You must be at least 18 years of age to register.");
+    return false;
+  }
+  else {
+    return true;
+  }
+
+}
+
+function pwMatch () {
+  console.log("pw")
+  var pw = document.getElementById("password").value;
+  var match = document.getElementById("confirmation").value;        
+
+  if (pw != match) {
+    alert("Password does not match!");
+    return false;
+  }
+  else {
+    return true;
+  }
+
+}
+
+function setTime() {
+  var time = Date.now();
+  document.getElementById("timestamp").value = time;
+}
+
+
+
+function validateLogin () {
+  if (document.getElementById("loginName").value.length === 0) {
+    alert("You must enter your login name.");
+    return;
+  }
+  else if (document.getElementById("loginPW").value.length ===0) {
+    alert("You must enter your password.");
+    return;
+  }
+  else {
+    document.location.href = "main.html";
+  }
+}
+
+function clearLogin () {
+  document.getElementById("loginName").value = "";
+  document.getElementById("loginPW").value = "";
+}
---
+++ inclass-9/src/vdom.js
@@ -0,0 +1,98 @@
+//
+// Inclass Virtual DOM Exercise
+// ============================
+//
+// You need to implement createElement() and updateElement()
+//
+;(function(exports) {
+
+'use strict'
+
+function h(tag, props, ...children) {
+    return { tag, props: props ? props : { }, 
+        children: Array.isArray(children[0]) ? children[0] : children }
+}
+
+function createElement(node) {
+	console.log('Create element called for', node)
+
+            var element;
+
+            if (node.children != null) {
+                element = document.createElement(node.tag);
+                for (var index in node.children) {
+                    var child = node.children[index];
+                    var childElement = createElement(child);
+                    element.appendChild(childElement);
+                }    
+            }
+            else {
+                element = document.createTextNode(node);
+            }
+
+            if (node.props != null && node.props.onClick != null) {
+                element.addEventListener("click",node.props.onClick);
+            }
+
+	// create the element and return it to the caller
+	// the node might have event listeners that need to be registered
+	// the node might have children that need to be created as well
+	return element;
+}
+
+function changed(node1, node2) {
+    return typeof node1 !== typeof node2 ||
+            (typeof node1 === 'string' && node1 !== node2) ||
+            node1.tag !== node2.tag ||
+            (node1.props && node2.props && 
+            	node1.props.id && node2.props.id && 
+            	node1.props.id != node2.props.id)
+}
+
+function updateElement(parent, newNode, oldNode, index=0) {
+	// index will be needed when you traverse children
+	// add the new node to the parent DOM element if
+	// the new node is different from the old node 
+	// at the same location in the DOM.
+	// ideally we also handle inserts, but ignore that functionality for now.
+
+    if (!oldNode) {
+        parent.appendChild(createElement(newNode))
+    } else {
+    	console.log('update element that may have changed')
+    	// you can use my changed(node1, node2) method above
+    	// to determine if an element has changed or not
+
+    	// be sure to also update the children!
+    }
+}
+
+const deepCopy = (obj) => {
+    if (obj === null || typeof(obj) !== 'object')
+        return obj;
+    const props = {}
+    if (obj.props) {
+        for (let p in obj.props) {
+            props[p] = obj.props[p]
+        }
+    }
+    return h(obj.tag, props,
+        Array.isArray(obj.children) ? obj.children.map(deepCopy) : obj.children)
+}
+
+const update = () => requestAnimationFrame(() => {
+	// compare the current vdom with the original vdom for updates
+    updateElement(h.mounted.root, h.mounted.current, h.mounted.original)
+    h.mounted.original = deepCopy(h.mounted.current)
+})
+
+h.mount = (root, component) => {
+    // we keep a copy of the original virtual DOM so we can diff it later for updates
+    const originalComponent = deepCopy(component)
+    h.mounted = { root: root, current: component, original: originalComponent }
+    updateElement(root, originalComponent)
+}
+
+exports.h = h
+
+})(window);

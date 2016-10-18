b6efdcfcd6a81df8d7819c7f5b71eced81748d1a
Suzanne Wen <suz.wen@yahoo.com>
2016-10-18 16:18:18 -0500

    fixed post request

--- inclass-16/index.js
+++ inclass-16/index.js
@@ -1,14 +1,19 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+let newArticle = {}
+let articles = [ { id:1, author: 'Scott', body:'A post' }, { id:2, author: 'Suzanne', body:'A post' }, { id:3, author: 'Wen', body:'A post' } ];
 
 const addArticle = (req, res) => {
-     console.log('Payload received', req.body)    
-     res.send(req.body)
+     console.log('Payload received', req.body)
+     const body = req.body.body
+     newArticle =  { id:4, author: 'Scott', body:body }
+     articles.push(newArticle)
+     res.send(JSON.stringify(newArticle))
 }
 
 const getArticles = (req, res) => {
-     const payload = { 'articles': [ { id:1, author: 'Scott', body:'A post' }, { id:2, author: 'Suzanne', body:'A post' }, { id:3, author: 'Wen', body:'A post' } ]};
+     const payload = { 'articles': articles };
      res.send(JSON.stringify(payload))
 }
 

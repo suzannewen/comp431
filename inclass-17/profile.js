8882bfac19aa13c12fbffa70608150c79081e9de
Suzanne Wen <suz.wen@yahoo.com>
2016-10-21 03:40:03 -0500

    fixed response

--- inclass-17/profile.js
+++ inclass-17/profile.js
@@ -4,19 +4,27 @@ const index = (req, res) => {
 }
 
 const getHeadline = (req, res) => {
-     res.send({ headlines: [{username: Scott, status: Happy }]})
+     res.send({ headlines: [{username: 'Scott', status: 'Happy' }]})
 }
 
 const getEmail = (req, res) => {
-     res.send({ username: Suzanne, email: suz.wen@yahoo.com })
+     res.send({ username: 'Suzanne', email: 'email' })
+}
+
+const getEmailUser = (req, res) => {
+     res.send({ username: 'user', email: 'emailAddress' })
 }
 
 const getZipcode = (req, res) => {
-     res.send({ username: Suzanne, zipcode: 76016 })
+     res.send({ username: 'Suzanne', zipcode: '76016' })
+}
+
+const getZipcodeUser = (req, res) => {
+     res.send({ username: 'user', zipcode: 'emailAddress' })
 }
 
 const getAvatar = (req, res) => {
-     res.send({ username: Suzanne, avatar: picture })
+     res.send({ username: 'Suzanne', avatar: 'picture' })
 }
 
 module.exports = app => {
@@ -25,4 +33,9 @@ module.exports = app => {
      app.get('/email', getEmail)
      app.get('/zipcode', getZipcode)
      app.get('/avatar', getAvatar)
+
+     app.get('/headlines/:user?', getHeadline)
+     app.get('/email/:user?', getEmailUser)
+     app.get('/zipcode/:user?', getZipcodeUser)
+     app.get('/avatar/:user?', getAvatar)
 }
